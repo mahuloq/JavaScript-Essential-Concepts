7b4d93c707621234e69e56641fcb7c876c6f55b0
Matt Tucker <arcanemahuloq@gmail.com>
2023-09-03 20:50:11 -0500

    Understood Variable Hoisting

---
+++ hoisting.js
@@ -0,0 +1,19 @@
+var dog;
+
+console.log(dog);
+dog = "Puppy";
+console.log(dog);
+
+console.log(cat);
+var cat = "Kitty";
+console.log(cat);
+
+console.log(animal);
+let animal;
+console.log(animal);
+animal = "dog";
+console.log(animal);
+
+console.log(food);
+const food = "biscuit";
+console.log(food);
